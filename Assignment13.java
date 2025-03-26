784c0cc010c884abb9380216a0a2b0752ba41bef
Athulya Pai <athulyaspai@gmail.com>
2025-03-26 11:46:51 +0530

    1ST PROGRAM

---
+++ Assignment13.java
@@ -0,0 +1,24 @@
+package package1;
+
+public class Assignment13 {
+	
+	public static void add () {
+		int a = 10;
+		int b= 20;
+		int sum = a+b;
+		System.out.println(sum);
+	}
+
+	  void sub () {
+		int a = 100;
+		int b= 50;
+		int sub = a-b;
+		System.out.println(sub);
+	}
+	public static void main(String[] args) {
+		Assignment13 x = new Assignment13();
+		x.sub();
+		add();
+	}
+
+}
